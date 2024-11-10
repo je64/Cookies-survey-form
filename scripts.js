1c27096ed98de8860a1743307c0176b891541c4e
je64 <elje065@gmail.com>
2024-11-10 13:41:13 +0800

    palpak

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
+
+function goTo() {
+  window.location.href = "index2.html"; // Replace with the URL of the next webpage
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('survey-form');
   const nextButton = document.getElementById('next');
 
-  function validateForm() {
+
+
+  form.addEventListener('input', function () {
     let allValid = true;
     form.querySelectorAll('input[required], select[required]').forEach(function (input) {
       if (!input.value) {
         allValid = false;
+        
       }
     });
-    return allValid;
-  }
 
-  form.addEventListener('input', function () {
-    nextButton.disabled = !validateForm();
-  });
+    nextButton.disabled = !allValid;
 
-  nextButton.addEventListener('click', function (event) {
-    if (validateForm()) {
-      window.location.href = "index2.html"; // Replace with the URL of the next webpage
-    } else {
-      alert('Please fill out all required fields.');
-    }
+    
   });
 });
+
